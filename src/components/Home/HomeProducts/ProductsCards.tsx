039b570aa0b8ab730a737dc1920c585ee17e2647
SergioJr <sergiojunior.dms@gmail.com>
2023-11-06 12:39:30 -0300

    hoje

--- src/components/Home/HomeProducts/ProductsCards.tsx
+++ src/components/Home/HomeProducts/ProductsCards.tsx
@@ -1,19 +1,30 @@
-interface ProductCardProps{
-    product_img: any
-    product_name: string
-    product_price: number
+import { IconHeart } from "@tabler/icons-react"
+import * as Toast from '@radix-ui/react-toast';
+import { useState } from "react";
+import ToastDemo from "./Toast";
+
+interface ProductCardProps {
+  product_img: any
+  product_name: string
+  product_price: number
 }
 
-export default function ProductsCards(props: ProductCardProps){
-    return(
-        <div className="bg-palette-light h-[23rem] hover:scale-110 duration-300 hover:cursor-pointer">
-          <div className="bg-palette-mediumlight w-72 h-72 text-black">
-             {props.product_img}
-          </div>
-          <div className="pt-2">
-            <p className="text-black">{props.product_name}</p>
-            <p className="text-palette-mediumgreen font-bold pt-2">R$ {props.product_price}</p>
-          </div>
-        </div>
-    )
+export default function ProductsCards(props: ProductCardProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="bg-palette-light h-[23rem] hover:scale-110 duration-300 hover:cursor-pointer">
+      <div className="bg-palette-mediumlight w-72 h-72 text-black">
+        {props.product_img}
+      </div>
+      <div className="pt-2">
+        <p className="text-black">{props.product_name}</p>
+        <span className="flex flex-row items-center justify-between">
+          <p className="text-palette-mediumgreen font-bold pt-2">R$ {props.product_price}</p>
+      
+          <IconHeart className="text-black mr-4 hover:scale-110 hover:text-red-600  duration-200" />
+        </span>
+      </div>
+    </div>
+  )
 }
--- src/components/Shop/ShopHeader.tsx
+++ src/components/Shop/ShopHeader.tsx
@@ -2,8 +2,21 @@ import { Montserrat2, MontserratAlternates } from "@/pages/_app";
 import Header from "../Home/Header/Header";
 import { IconChevronRight } from "@tabler/icons-react";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function ShopHeader() {
+    useEffect(() => {
+        const replacers = document.querySelectorAll('[data-replace]');
+        for (let i = 0; i < replacers.length; i++) {
+          const replaceClasses = JSON.parse(replacers[i].getAttribute('data-replace')!.replace(/'/g, '"'));
+          Object.keys(replaceClasses).forEach((key) => {
+            replacers[i].classList.remove(key);
+            replacers[i].classList.add(replaceClasses[key]);
+          });
+        }
+      }, []);
+
+
     return (
         <div className={`
         flex flex-col gap-44
@@ -16,10 +29,18 @@ export default function ShopHeader() {
             </header>
 
             <section className="flex flex-col items-center gap-4 text-palette-light">
-                <h1 className={`font-montserrat_alternate text-7xl ${MontserratAlternates.variable}`}>
+                <h1 className={`
+                    font-montserrat_alternate text-7xl ${MontserratAlternates.variable}
+                    transform transition-all translate-y-[-100%] ease-out duration-300
+                `} 
+                data-replace='{ "translate-y-[-100%]": "translate-x-0" }'>
                     Produtos
                 </h1>
-                <p className={`flex flex-row items-center text-xl font-montserrat_alternate ${Montserrat2.variable}`}>
+                <p className={`
+                    flex flex-row items-center text-xl font-montserrat_alternate ${Montserrat2.variable}
+                    transform transition-all translate-y-[-100%] ease-out duration-500
+                `}
+                data-replace='{ "translate-y-[-100%]": "translate-x-0" }'>
                     <Link href={'./home'} className="hover:text-palette-mediumlight duration-300">Home</Link> <IconChevronRight /> Shop
                 </p>
             </section>
