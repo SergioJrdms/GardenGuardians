3997546b04d6b5dffae6954af3dc4cb8dc174f17
SergioJr <sergiojunior.dms@gmail.com>
2023-11-17 19:53:58 -0300

    sexta

--- src/pages/garden-guardians/history/index.tsx
+++ src/pages/garden-guardians/history/index.tsx
@@ -45,40 +45,40 @@ export default function history() {
                     <hr className="bg-palette-mediumgreen h-1 w-40" />
                 </section>
 
-                <section className="flex justify-center items-center pt-10">
+                <section className="flex justify-center items-center ">
                     <div className="grid text-black grid-cols-3 gap-16 select-none">
 
-                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh]">
+                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh] group">
                             <div className="flex flex-col gap-8">
                                 <span className="flex flex-col justify-center items-center">
-                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable}`}>Nossa história</p>
+                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable} group-hover:scale-105 duration-300`}>Nossa história</p>
                                     <hr className="bg-palette-mediumgreen h-[0.3vh] w-[7vh]" />
                                 </span>
-                                <p className="flex justify-center text-lg">
+                                <p className="flex justify-center text-lg text-pale text-center">
                                     Sophia Greenfield, uma entusiasta da natureza, deu vida à GardenGuardians, uma empresa dedicada a transformar cada lar em um paraíso verde. Com uma visão clara de conectar as pessoas à beleza da natureza, a GardenGuardians oferece uma variedade exuberante de plantas, ferramentas de jardinagem de alta qualidade e serviços especializados. Desde flores exóticas até consultorias personalizadas, a empresa visa inspirar, educar e capacitar, tornando-se um farol verde para aqueles que buscam cultivar seus próprios oásis de serenidade em meio à agitação da vida moderna. Junte-se à GardenGuardians e descubra a alegria de ser o guardião do seu próprio jardim, onde a natureza encontra um lar acolhedor em cada ambiente.
                                 </p>
                             </div>
                         </div>
 
-                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh]">
+                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh] group">
                             <div className="flex flex-col gap-8">
                                 <span className="flex flex-col justify-center items-center">
-                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable}`}>Porque nos escolher?</p>
+                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable} group-hover:scale-105 duration-300`}>Porque nos escolher?</p>
                                     <hr className="bg-palette-mediumgreen h-[0.3vh] w-[10vh]" />
                                 </span>
-                                <p className="flex justify-center text-lg">
+                                <p className="flex justify-center text-lg text-center">
                                     Ao decidir criar seu próprio refúgio verde, escolher a GardenGuardians significa optar pela excelência e paixão pela jardinagem. Nossa empresa, fundada por Sophia Greenfield e sua equipe de entusiastas, não apenas oferece uma seleção cuidadosamente curada de plantas exuberantes, ferramentas de jardinagem ergonômicas e acessórios encantadores, mas também proporciona uma experiência holística para transformar sua visão em realidade. Com a GardenGuardians, você não apenas compra produtos, mas adquire uma parceria dedicada a inspirar, educar e apoiar, tornando-se o guardião de seu próprio oásis verde. Escolha a GardenGuardians e descubra como nossa paixão pela natureza e compromisso com a qualidade podem fazer a diferença em cada jardim, elevando sua experiência de jardinagem a novos patamares.
                                 </p>
                             </div>
                         </div>
 
-                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh]">
+                        <div className="border border-zinc-300 hover:border-zinc-800 duration-300 p-10 rounded-lg w-[50vh] group">
                             <div className="flex flex-col gap-8">
                                 <span className="flex flex-col justify-center items-center">
-                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable}`}>Nosso futuro</p>
+                                    <p className={`font-extrabold font-montserrat_alternate text-2xl ${Montserrat2.variable} group-hover:scale-105 duration-300`}>Nosso futuro</p>
                                     <hr className="bg-palette-mediumgreen h-[0.3vh] w-[5vh]" />
-                                </span>
-                                <p className="flex justify-center text-lg">
+                                </span> 
+                                <p className="flex justify-center text-lg text-center">
                                     Enquanto a GardenGuardians floresce no presente, nosso olhar está firmemente fixado em um futuro repleto de crescimento e inovação. Nossa visão é expandir as fronteiras do verde, trazendo mais diversidade de plantas e produtos inovadores para os lares ao redor do mundo. Planejamos lançar iniciativas educacionais ainda mais robustas, oferecendo tutoriais interativos, workshops e recursos inspiradores para fortalecer a conexão das pessoas com a jardinagem. Além disso, estamos explorando parcerias sustentáveis e práticas ambientais para garantir que, à medida que prosperamos, continuemos sendo guardiões responsáveis da natureza. Na GardenGuardians, o futuro se desenha com paisagens ainda mais vibrantes, jardins florescentes e uma comunidade global unida pelo amor à natureza. Junte-se a nós enquanto cultivamos um futuro onde cada lar é um testemunho vivo da beleza e da serenidade que a natureza proporciona.
                                 </p>
                             </div>
--- src/pages/garden-guardians/login/index.tsx
+++ src/pages/garden-guardians/login/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { IconChevronLeft } from "@tabler/icons-react";
+import { IconBrandGmail, IconChevronLeft } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function login() {
@@ -23,28 +23,34 @@ export default function login() {
                                 <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-palette-lightgreen">
                                     <div className="relative flex items-center space-x-4 justify-center">
                                         <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo" />
-                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue with Google</span>
+                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue com o Google</span>
                                     </div>
                                 </button>
                                 <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-palette-lightgreen">
                                     <div className="relative flex items-center space-x-4 justify-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="absolute left-0 w-5 text-gray-700" viewBox="0 0 16 16">
-                                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                                        <svg xmlns="https://icons8.com/icon/eFPBXQop6V2m/gmail-logo.svg" fill="currentColor" className="absolute left-0 w-5 text-gray-700" viewBox="0 0 16 16">
+                                            <IconBrandGmail />
                                         </svg>
-                                        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue with Github</span>
+                                        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue com o Gmail</span>
                                     </div>
                                 </button>
                                 <button className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-palette-lightgreen">
                                     <div className="relative flex items-center space-x-4 justify-center">
                                         <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" className="absolute left-0 w-5" alt="Facebook logo" />
-                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue with Facebook</span>
+                                            <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-palette-lightgreen sm:text-base">Continue com o Facebook</span>
                                     </div>
                                 </button>
                             </div>
 
+                            <div className="pt-6">
+                                <Link href={"./cadastro"}>
+                                    <p className="text-palette-dark hover:text-palette-mediumgreen duration-300 text-lg">Ainda não tem uma conta? Clique aqui e faça uma!</p>
+                                </Link>
+                            </div>
+
                             <div className="mt-32 space-y-4 text-gray-600 text-center sm:-mb-8">
-                                <p className="text-xs">Ao prosseguir você concorda com nossos  <a href="#" className="underline">Termos de Uso</a> e cofirma a leitura do <a href="#" className="underline">Privacidade & Cookies</a>.</p>
-                                <p className="text-xs">Este site é protegido por reCAPTCHA e o <a href="#" className="underline">Google Privacy Policy</a> e <a href="#" className="underline">Termos de Serviço</a></p>
+                                <p className="text-xs">Ao prosseguir você concorda com nossos  <a href="#" className="underline hover:text-palette-lightgreen duration-300">Termos de Uso</a> e cofirma a leitura do <a href="#" className="underline hover:text-palette-lightgreen duration-300">Privacidade & Cookies</a>.</p>
+                                <p className="text-xs">Este site é protegido por reCAPTCHA e o <a href="#" className="underline hover:text-palette-lightgreen duration-300">Google Privacy Policy</a> e <a href="#" className="underline hover:text-palette-lightgreen duration-300">Termos de Serviço</a></p>
                             </div>
                         </div>
                     </div>
